ce73cd9ba24ac992e44e036be9968029075568ac
Morten <moje62379@edu.ucl.dk>
2024-12-04 17:20:51 +0100

    NavigateBackToMainMenu

--- RandersKFUM/ViewModel/AdministrationViewModel.cs
+++ RandersKFUM/ViewModel/AdministrationViewModel.cs
@@ -13,11 +13,13 @@ namespace RandersKFUM.ViewModel
     {
         public RelayCommand NavigateToManageTeamViewCommand { get; }
         public RelayCommand NavigateToManageTeamLeaderCommand { get; }
+        public RelayCommand NavigateBackToMainMenuCommand { get; }
 
         public AdministrationViewModel()
         {
             NavigateToManageTeamViewCommand = new RelayCommand(_ => NavigateToManageTeamView());
             NavigateToManageTeamLeaderCommand = new RelayCommand(_ => NavigateToTeamLeaderView());
+            NavigateBackToMainMenuCommand = new RelayCommand(_ => NavigateBackToMainMenuView());
         }
 
         private void NavigateToManageTeamView()
@@ -28,6 +30,11 @@ namespace RandersKFUM.ViewModel
         private void NavigateToTeamLeaderView()
         {
             RandersKFUM.Utilities.NavigationService.NavigateTo(new ManageTeamLeaderView());
+        }
+
+        private void NavigateBackToMainMenuView()
+        {
+            RandersKFUM.Utilities.NavigationService.NavigateTo(new MainMenuView());
         }
     }
 }
--- RandersKFUM/ViewModel/BookingOverviewViewModel.cs
+++ RandersKFUM/ViewModel/BookingOverviewViewModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RandersKFUM.Utilities;
+using RandersKFUM.View;
 
 namespace RandersKFUM.ViewModel
 {
     internal class BookingOverviewViewModel
     {
+        public RelayCommand NavigateBackToMainMenuCommand { get; }
+
+        public BookingOverviewViewModel() 
+        {
+            NavigateBackToMainMenuCommand = new RelayCommand(_ => NavigateBackToMainMenuView());
+        }
+
+        private void NavigateBackToMainMenuView()
+        {
+            RandersKFUM.Utilities.NavigationService.NavigateTo(new MainMenuView());
+        }
+
     }
 }
